ad048e5b5dc60d79ebd53d37908449d19bd4eaf4
Jessica Lane <jessica.lynn.brasher@gmail.com>
2016-04-28 19:24:31 -0400

    Updated styles

--- inputcontroller.js
+++ inputcontroller.js
@@ -4,19 +4,16 @@ app.controller('inputcontroller', function($scope){
 	
 	$scope.verify = function(number){
 		var number = $scope.number;
-				if (number%3===0 && number%5 === 0){	$scope.result = "FizzBuzz";
-				}
-				else if (number%5 === 0){
+				if (number%3===0 && number%5 === 0) {
+					$scope.result = "FizzBuzz";
+				} else if (number%5 === 0) {
 					$scope.result = "Buzz";
-				}
-				else if (number%3 === 0){
+				} else if (number%3 === 0) {
 					$scope.result = "Fizz";					
-				}
-				else {
+				} else {
 					$scope.result = "Your number is not divisble by 3 or 5.  Try again!";
-				}
-			
-		};
+				}	
+			};
 		
 		});
 
