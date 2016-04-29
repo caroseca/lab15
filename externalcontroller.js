00e37eeea305c540a0871c322adfde2b40bb298d
Michael Lawrence <m6lawrence1@gmail.com>
2016-04-28 21:23:31 -0400

    Fixed external partial controller

--- externalcontroller.js
+++ externalcontroller.js
@@ -1,26 +1,3 @@
-app.controller('externalcontroller',
-	function($scope, $timeout, githubService){
-		$scope.events =
-			githubService.events('auser');
-	});
-
-app.controller('externalcontroller',
-	function($scope, githubService) {
-		$scope.setUsername =
-			githubService.setUsername;
-// watch for changes on the username property if there is a change, run the function
-	$scope.$watch('username', function(newUsername) {
-// uses the $http service to call the GitHub API and returns the resulting promise
-		githubService.events(newUsername)
-		.success(function(data, status, headers) {
-			// the success function wraps
-			// the response in data
-			// so we need to call data.data to 
-			// fetch the raw data
-			$scope.events = data.data;
-		})
-	});
-});
 
 app.controller('externalcontroller',
 	function($scope, $timeout, githubService) {
